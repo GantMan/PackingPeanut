98d097e5d107c4c5a8107482c7178ae3ee6375ff
Gant <GantMan@gmail.com>
2015-06-25 22:26:34 -0500

    added friendly aliases

--- lib/PackingPeanut-android/PackingPeanut.rb
+++ lib/PackingPeanut-android/PackingPeanut.rb
@@ -1,105 +1,109 @@
-module App
-  module Persistence
+module PackingPeanut
 
-    MODE_PRIVATE = 0
-    MODE_WORLD_READABLE = 1
-    MODE_WORLD_WRITEABLE = 2
-    MODE_MULTI_PROCESS = 4
+  MODE_PRIVATE = 0
+  MODE_WORLD_READABLE = 1
+  MODE_WORLD_WRITEABLE = 2
+  MODE_MULTI_PROCESS = 4
 
-    JSONObject = Org::Json::JSONObject
+  JSONObject = Org::Json::JSONObject
 
-    PREFERENCE_MODES = {
-      private: MODE_PRIVATE,
-      readable: MODE_WORLD_READABLE,
-      world_readable: MODE_WORLD_READABLE,
-      writable: MODE_WORLD_WRITEABLE,
-      world_writable: MODE_WORLD_WRITEABLE,
-      multi: MODE_MULTI_PROCESS,
-      multi_process: MODE_MULTI_PROCESS
-    }
+  PREFERENCE_MODES = {
+    private: MODE_PRIVATE,
+    readable: MODE_WORLD_READABLE,
+    world_readable: MODE_WORLD_READABLE,
+    writable: MODE_WORLD_WRITEABLE,
+    world_writable: MODE_WORLD_WRITEABLE,
+    multi: MODE_MULTI_PROCESS,
+    multi_process: MODE_MULTI_PROCESS
+  }
 
-    module_function
+  module_function
 
-    # Serialize key/value as json then
-    # store that string with the settings key == json key
-    def []=(key, value)
-      # Let's play nice with strings and non-strings
-      string_key = key.to_s
-      settings = get_settings
-      editor = settings.edit
-      json = serialize(string_key,value)
-      editor.putString(string_key, json.toString)
-      editor.commit
-    end
-
-    def [](key)
-      # Let's play nice with strings and non-strings
-      string_key = key.to_s
-      json_string = get_value(string_key)
-      return json_string if json_string == ""
-      deserialize(string_key, json_string)
-    end
-
-    def serialize(key, value)
-      Moran.generate({:"#{key}" => value})
-    end
-
-    def deserialize(key, json_string)
-      Moran.parse(json_string)[key]
-    end
-
-    def get_value key
-      settings = get_settings
-      settings.getString(key,nil)
-    end
-
-    def storage_file=(value)
-      @persistence_storage_file = value
-    end
-
-    def storage_file
-      @persistence_storage_file ||= "default_persistence_file"
-    end
-
-    def preference_mode=(value)
-      @current_preference_mode = PREFERENCE_MODES[value] || value
-    end
-
-    def preference_mode
-      @current_preference_mode ||= MODE_PRIVATE
-    end
-
-    def all
-      settings = get_settings
-      all_hashes = settings.getAll.map { |key, value| Moran.parse(value) }
-
-      # Currently an array of hashes, needs to be one big hash
-      merged_hashes = {}
-      all_hashes.each do |h|
-        merged_hashes = merged_hashes.merge(h)
-      end
-
-      merged_hashes
-    end
-
-    def get_settings
-      current_context.getSharedPreferences(storage_file, preference_mode)
-    end
-
-    # Allows us to use this from anywhere by setting the context
-    # Useful when you want to access this module from the REPL
-    def current_context
-      #p defined? getApplicationContext
-      @context || getApplicationContext
-    end
-
-    # attr_accessor is not supported for modules in RMAndroid... yet.
-    def context= supplied_context
-      @context = supplied_context
-    end
-
+  # Serialize key/value as json then
+  # store that string with the settings key == json key
+  def []=(key, value)
+    # Let's play nice with strings and non-strings
+    string_key = key.to_s
+    settings = get_settings
+    editor = settings.edit
+    json = serialize(string_key,value)
+    editor.putString(string_key, json.toString)
+    editor.commit
   end
+
+  def [](key)
+    # Let's play nice with strings and non-strings
+    string_key = key.to_s
+    json_string = get_value(string_key)
+    return nil if json_string == ""
+    deserialize(string_key, json_string)
+  end
+
+  def serialize(key, value)
+    Moran.generate({:"#{key}" => value})
+  end
+
+  def deserialize(key, json_string)
+    Moran.parse(json_string)[key]
+  end
+
+  def get_value key
+    settings = get_settings
+    settings.getString(key,nil)
+  end
+
+  def storage_file=(value)
+    @persistence_storage_file = value
+  end
+
+  def storage_file
+    @persistence_storage_file ||= "default_persistence_file"
+  end
+
+  def preference_mode=(value)
+    @current_preference_mode = PREFERENCE_MODES[value] || value
+  end
+
+  def preference_mode
+    @current_preference_mode ||= MODE_PRIVATE
+  end
+
+  def all
+    settings = get_settings
+    all_hashes = settings.getAll.map { |key, value| Moran.parse(value) }
+
+    # Currently an array of hashes, needs to be one big hash
+    merged_hashes = {}
+    all_hashes.each do |h|
+      merged_hashes = merged_hashes.merge(h)
+    end
+
+    merged_hashes
+  end
+
+  def get_settings
+    current_context.getSharedPreferences(storage_file, preference_mode)
+  end
+
+  # Allows us to use this from anywhere by setting the context
+  # Useful when you want to access this module from the REPL
+  def current_context
+    # THIS SHOULD HAVE SAFE FAILOVERS
+    # BUT defined? is causing erros in RMA as of this commit
+    @context
+  end
+
+  # attr_accessor is not supported for modules in RMAndroid... yet.
+  def context= supplied_context
+    @context = supplied_context
+  end
+
 end
 
-# delicious shortcut
-PP = App::Persistence unless defined? PP
+# delicious PP shortcut
+PP = PackingPeanut unless defined? PP
+
+# Bubblewrap syntax
+module App
+  Persistence = PackingPeanut unless defined? App::Persistence
+end
--- lib/PackingPeanut-ios/PackingPeanut.rb
+++ lib/PackingPeanut-ios/PackingPeanut.rb
@@ -1,68 +1,71 @@
 # Persistence module built on top of NSUserDefaults
-module App
-  module Persistence
-    module_function
+module PackingPeanut
+  module_function
 
-    def app_key
-      @app_key ||= NSBundle.mainBundle.bundleIdentifier
-    end
-
-    def storage_file
-      @persistence_storage_file ||= "default_persistence_file"
-    end
-
-    def storage_file=(value)
-      @persistence_storage_file = value
-    end
-
-    def []=(key, value)
-      storage.setObject(value, forKey: storage_key(key))
-      storage.synchronize
-    end
-
-    def [](key)
-      value = storage.objectForKey storage_key(key)
-
-      # RubyMotion currently has a bug where the strings returned from
-      # standardUserDefaults are missing some methods (e.g. to_data).
-      # And because the returned object is slightly different than a normal
-      # String, we can't just use `value.is_a?(String)`
-      value.class.to_s == 'String' ? value.dup : value
-    end
-
-    def merge(values)
-      values.each do |key, value|
-        storage.setObject(value, forKey: storage_key(key))
-      end
-      storage.synchronize
-    end
-
-    def delete(key)
-      value = storage.objectForKey storage_key(key)
-      storage.removeObjectForKey(storage_key(key))
-      storage.synchronize
-      value
-    end
-
-    def storage
-      NSUserDefaults.standardUserDefaults
-    end
-
-    def storage_key(key)
-      "#{app_key}_#{storage_file}_#{key}"
-    end
-
-    def all
-      hash = storage.dictionaryRepresentation.select{|k,v| k.start_with?("#{app_key}_#{storage_file}") }
-      new_hash = {}
-      hash.each do |k,v|
-        new_hash[k.sub("#{app_key}_#{storage_file}_", '')] = v
-      end
-      new_hash
-    end
+  def app_key
+    @app_key ||= NSBundle.mainBundle.bundleIdentifier
   end
 
+  def storage_file
+    @persistence_storage_file ||= "default_persistence_file"
+  end
+
+  def storage_file=(value)
+    @persistence_storage_file = value
+  end
+
+  def []=(key, value)
+    storage.setObject(value, forKey: storage_key(key))
+    storage.synchronize
+  end
+
+  def [](key)
+    value = storage.objectForKey storage_key(key)
+
+    # RubyMotion currently has a bug where the strings returned from
+    # standardUserDefaults are missing some methods (e.g. to_data).
+    # And because the returned object is slightly different than a normal
+    # String, we can't just use `value.is_a?(String)`
+    value.class.to_s == 'String' ? value.dup : value
+  end
+
+  def merge(values)
+    values.each do |key, value|
+      storage.setObject(value, forKey: storage_key(key))
+    end
+    storage.synchronize
+  end
+
+  def delete(key)
+    value = storage.objectForKey storage_key(key)
+    storage.removeObjectForKey(storage_key(key))
+    storage.synchronize
+    value
+  end
+
+  def storage
+    NSUserDefaults.standardUserDefaults
+  end
+
+  def storage_key(key)
+    "#{app_key}_#{storage_file}_#{key}"
+  end
+
+  def all
+    hash = storage.dictionaryRepresentation.select{|k,v| k.start_with?("#{app_key}_#{storage_file}") }
+    new_hash = {}
+    hash.each do |k,v|
+      new_hash[k.sub("#{app_key}_#{storage_file}_", '')] = v
+    end
+    new_hash
+  end
 end
 
-# delicious shortcut
-PP = App::Persistence unless defined? PP
+
+# delicious PP shortcut
+PP = PackingPeanut unless defined? PP
+
+# Bubblewrap syntax
+module App
+  Persistence = PackingPeanut unless defined? App::Persistence
+end
